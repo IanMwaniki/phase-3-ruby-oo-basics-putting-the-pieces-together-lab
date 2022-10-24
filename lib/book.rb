6aa3a315d3e53ade611bfe4a1a68dde3ca90d42b
Ian Mwaniki <ian.mwaniki@student.moringaschool.com>
2022-10-24 19:45:07 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
-
+class Book
+    attr_accessor :genre, :title,
+    :author, :page_count
+    def initialize(title)
+        @title=title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+book = Book.new("The World According to Garp")
+book.turn_page
 
